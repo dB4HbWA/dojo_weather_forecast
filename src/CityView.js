7f55f36cd0030fe47ab9ba845aad67e094a646e9
Vince Carini <vincecarini@northwesternmutual.com>
2018-02-09 16:31:12 -0600

    All cities showing

--- src/CityView.js
+++ src/CityView.js
@@ -4,15 +4,21 @@ import { loadWeather, TOGGLE_FAHRENHEIT } from "./state/action";
 
 class CityView extends Component {
 
-  
-
-  componentWillReceiveProps(nextProps) {
-    if (this.props.match === nextProps.match) return
+  componentWillMount() {
     const cityToDisplay = this.props.locations.find((location) => (location.nameInURL === this.props.match.params.name))
-    console.log(cityToDisplay);
+   
 
     if (cityToDisplay)
       this.props.loadWeatherToCityView(cityToDisplay.id)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.match === nextProps.match) return
+    const cityToDisplay = this.props.locations.find((location) => (location.nameInURL === nextProps.match.params.name))
+    console.log(cityToDisplay);
+
+    if (cityToDisplay)
+      nextProps.loadWeatherToCityView(cityToDisplay.id)
   }
 
   render() {
@@ -23,7 +29,7 @@ class CityView extends Component {
       <div className='cityView card' style={{ width: '700px' }}>
         <div className='row'>
           <div className="cityView" style={{ width: '100%' }}>
-            <h1>San Jose, CA</h1>
+            <h1>{this.props.weatherLoadingStatus === "success" && this.props.weatherDataOnView && this.props.weatherDataOnView.name}</h1>
             <div className="small-6 large-5 columns">
               <div>Humidity: {this.props.weatherLoadingStatus === "success" && this.props.weatherDataOnView && this.props.weatherDataOnView.main.humidity}</div>
               <div>Temperature (High): {this.props.weatherLoadingStatus === "success" && this.props.weatherDataOnView && Math.round(this.props.weatherDataOnView.main.temp_max)}</div>
--- src/state/store.js
+++ src/state/store.js
@@ -4,7 +4,8 @@ import thunk from 'redux-thunk'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-localStorage.setItem("apiKey","9e48d00c9be7e826c8586bf11f8dd849")
+//localStorage.setItem("apiKey","9e48d00c9be7e826c8586bf11f8dd849")
+localStorage.setItem("apiKey","e732348669ce5455d2c70577462ce33b")
 
 export default createStore(
    reducer,
