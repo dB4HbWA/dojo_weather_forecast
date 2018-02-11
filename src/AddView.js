ef2b5bec181ed1d5d8e528700e2da961a03f3964
Vince Carini <vincecarini@northwesternmutual.com>
2018-02-11 13:05:05 -0600

    Add screen looks ok now

--- src/AddView.js
+++ src/AddView.js
@@ -1,13 +1,21 @@
 import React from 'react';
 const AddView = props => (
     <div className='addView card' style={{ width: '700px' }}>
-        <div style={{width: '100%'}}>
+        <div className="addHeader">
             <h1>Add a City</h1>
         </div>
-        <div>
-            <input type="text" placeholder="Enter a city" />
-            <button>Add a City</button>
+        <div className="row">
+            <div className="small-1 medium-1 large-1 columns">&nbsp;</div>
+            <div className="small-6 medium-6 large-6 columns">
+                <input type="text" placeholder="Enter a city" />
+            </div>
+            <div className="small-3 medium-3 large-3 columns">
+                <button>Add a City</button>
+            </div>
+            <div className="small-2 medium-2 large-2 columns">&nbsp;</div>
         </div>
+
+
     </div>
 )
 
